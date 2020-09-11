3b0a1ae64bdbd88b454fcb38884cb6c6fe03c930
hereiam123 <bdemaio1@gmail.com>
2020-09-10 23:35:47 -0400

    Changing bot permissions in test server

--- src/bot.js
+++ src/bot.js
@@ -5,6 +5,9 @@ const { Client } = require("discord.js");
 
 const client = new Client();
 
+//Command prefix
+const PREFIX = "$";
+
 //Login bot
 client.login(process.env.DISCORD_TOKEN);
 
@@ -17,6 +20,26 @@ client.on("ready", () => {
 client.on("message", (message) => {
   console.log(`${message.author.tag} said ${message.content}`);
   if (message.author.bot) return;
-  message.reply("What's up?");
-  message.channel.send("Nah brah");
+  //Trigger on command
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+
+    //Respond to a kick message, and try to find if user exists
+    //If so, kick user
+    //Need to provide bot permissions in server for bot to kick
+    if (CMD_NAME === "kick") {
+      if (args.length === 0) return message.reply(`Please provide an ID.`);
+      const member = message.guild.members.cache.get(args[0]);
+      if (member) {
+        member.kick();
+      } else {
+        message.channel.send("Not a valid member ID");
+      }
+    } else if (CMD_NAME === "ban") {
+      message.channel.send("Banned a user");
+    }
+  }
 });
